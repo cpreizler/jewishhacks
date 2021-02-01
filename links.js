792e1158a524b46e48519bfecf7f46dd79ee6614
cpreizler <50078832+cpreizler@users.noreply.github.com>
2021-02-01 14:13:19 -0500

    Delete links.js

--- links.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {  
-    //document.getElementById('amznlink').click(); 
-    window.location.replace('https://amzn.to/3psqgnp');
-}
-
-
-/* if(document.URL.indexOf("#click_approved") >= 0){ 
-    document.getElementById('amznlink').click();
-} */
