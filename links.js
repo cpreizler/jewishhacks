b4d1b8169bb97a41b627f68c0c49f70dcbc7f773
cpreizler <50078832+cpreizler@users.noreply.github.com>
2021-05-21 00:01:48 -0400

    Update links.js

--- links.js
+++ links.js
@@ -30,6 +30,9 @@ window.onload = function () {
     else if(document.URL.indexOf("#uvsan") >= 0){ 
         window.location.replace('https://amzn.to/3oJK1Fu');
     }
+    else if(document.URL.indexOf("#mosquito") >= 0){ 
+        window.location.replace('https://amzn.to/3wl3e4K');
+    }
     else if(document.URL.indexOf("#wiresleeve") >= 0){ 
         window.location.replace('https://amzn.to/3xy0t1s');
     }
