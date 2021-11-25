2e747bbe43078b52ab481f3742ed8c985db232d4
cpreizler <50078832+cpreizler@users.noreply.github.com>
2021-11-25 13:57:03 -0500

    Update links.js

--- links.js
+++ links.js
@@ -30,6 +30,10 @@ else if(document.URL.indexOf("#bin") >= 0){
         window.location.replace('https://amzn.to/3sExKWt');
     }
 
+else if(document.URL.indexOf("#eraseliner") >= 0){ 
+        window.location.replace('https://amzn.to/3FNUTv7');
+    }
+
 else if(document.URL.indexOf("#stroller") >= 0){ 
         window.location.replace('https://amzn.to/3oWVO5L');
     }
