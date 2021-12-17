33957c35270b591ecdf00f389f66b5e9eb961629
cpreizler <50078832+cpreizler@users.noreply.github.com>
2021-12-16 20:04:18 -0500

    Update links.js

--- links.js
+++ links.js
@@ -39,6 +39,10 @@ else if(document.URL.indexOf("#lego") >= 0){
         window.location.replace('https://amzn.to/3xNBjfD');
     }
 
+else if(document.URL.indexOf("#drain") >= 0){ 
+        window.location.replace('https://amzn.to/30xt2QP');
+    }
+
 else if(document.URL.indexOf("#paintpen") >= 0){ 
         window.location.replace('https://amzn.to/3EJ2VFy');
     }
