2975a9bffc5389d8859a0b050cbbe9d51cfaef6d
cpreizler <50078832+cpreizler@users.noreply.github.com>
2022-02-03 18:52:29 -0500

    Update links.js

--- links.js
+++ links.js
@@ -34,6 +34,10 @@ else if(document.URL.indexOf("#shower") >= 0){
         window.location.replace('https://amzn.to/3qK6rLp');
     }
 
+else if(document.URL.indexOf("#box") >= 0){ 
+        window.location.replace('https://amzn.to/34dB8Qx');
+    }
+
 else if(document.URL.indexOf("#makeuporg") >= 0){ 
         window.location.replace('https://amzn.to/3IohfVu');
     }
