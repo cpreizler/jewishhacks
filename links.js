f33d1806a189cf5a2b4404071a94a5be6ccb9643
cpreizler <50078832+cpreizler@users.noreply.github.com>
2022-03-23 21:57:33 -0400

    Update links.js

--- links.js
+++ links.js
@@ -1,40 +1,4 @@
-window.onload = function () {  
-
-    document.getElementById("goo").onclick = function(){
-        window.location.replace('https://amzn.to/3psqgnp');
-    }
-
-    document.getElementById("carfill").onclick = function(){
-        window.location.replace('https://amzn.to/2M1Tf2c');
-    }
-
-    document.getElementById("cutboard").onclick = function(){
-        window.location.replace('https://amzn.to/2YlfT8O');
-    }
-
-    if(document.URL.indexOf("#goo") >= 0){ 
-        document.getElementById("goo").click();
-    }
-    else if(document.URL.indexOf("#carfill") >= 0){ 
-        document.getElementById("carfill").click();
-    }
-
-    else if(document.URL.indexOf("#cutboard") >= 0){ 
-        document.getElementById("cutboard").click();
-    }
-    else if(document.URL.indexOf("#towelbar") >= 0){ 
-        window.location.replace('https://amzn.to/3rgBaNx');
-    }
-
-else if(document.URL.indexOf("#bin") >= 0){ 
-        window.location.replace('https://amzn.to/3sExKWt');
-    }
-
-else if(document.URL.indexOf("#walker") >= 0){ 
-        window.location.replace('https://amzn.to/3M1Q0lV');
-    }
-
-else if(document.URL.indexOf("#foodtop") >= 0){ 
+if(document.URL.indexOf("#foodtop") >= 0){ 
         window.location.replace('https://amzn.to/3I67Y3J');
     }
 
@@ -72,6 +36,10 @@ else if(document.URL.indexOf("#pizzabox") >= 0){
 
 else if(document.URL.indexOf("#pizzatop") >= 0){ 
         window.location.replace('https://amzn.to/36ALH0M');
+    }
+
+else if(document.URL.indexOf("#carpetvacuum") >= 0){ 
+        window.location.replace('https://amzn.to/3tyxTwM');
     }
 
 else if(document.URL.indexOf("#footprint") >= 0){ 
